f53dea134a33491f3aafb0c65efb4f22f7028a0d
gauravkmr0110 <kmrgaurav0101@gmail.com>
2026-07-23 00:38:40 +0530

    Adding controller

---
+++ src/main/java/com/gaurav/urlshortener/controller/UrlShortenerController.java
@@ -0,0 +1,40 @@
+package com.gaurav.urlshortener.controller;
+
+import com.gaurav.urlshortener.dto.request.ShortenUrlRequest;
+import com.gaurav.urlshortener.dto.response.ShortenUrlResponse;
+import com.gaurav.urlshortener.service.UrlShortenerService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+
+@RestController
+@RequiredArgsConstructor
+public class UrlShortenerController {
+
+    private final UrlShortenerService urlShortenerService;
+
+    @PostMapping("/shorten")
+    public ResponseEntity<ShortenUrlResponse> shortenUrl(
+            @Valid @RequestBody ShortenUrlRequest request) {
+
+        ShortenUrlResponse response = urlShortenerService.shortenUrl(request);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+    @GetMapping("/{code}")
+    public ResponseEntity<Void> redirect(@PathVariable String code) {
+
+        String originalUrl = urlShortenerService.getOriginalUrl(code);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setLocation(URI.create(originalUrl));
+
+        return new ResponseEntity<>(headers, HttpStatus.MOVED_PERMANENTLY);
+    }
+}
