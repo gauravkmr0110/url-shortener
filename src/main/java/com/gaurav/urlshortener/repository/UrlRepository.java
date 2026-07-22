5517341710bde64e649b017cf9968f3767f4d983
gauravkmr0110 <kmrgaurav0101@gmail.com>
2026-07-22 21:14:08 +0530

    repository interface

---
+++ src/main/java/com/gaurav/urlshortener/repository/UrlRepository.java
@@ -0,0 +1,15 @@
+package com.gaurav.urlshortener.repository;
+
+import com.gaurav.urlshortener.entity.UrlMapping;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UrlRepository extends JpaRepository<UrlMapping, Long> {
+
+    Optional<UrlMapping> findByShortCode(String shortCode);
+
+    Optional<UrlMapping> findByOriginalUrl(String originalUrl);
+
+    boolean existsByShortCode(String shortCode);
+}
